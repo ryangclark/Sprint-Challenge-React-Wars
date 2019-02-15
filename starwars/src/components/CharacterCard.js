fe2b193d2e86782ac1dc69d0cdc25847d3fba123
Clark <rclark010@gmail.com>
2019-02-15 09:42:12 -0700

    Completed MVP.

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -6,7 +6,10 @@ const CharacterCard = props => {
         <div className="character-card">
             <h3>{props.character.name}</h3>
             {/* <p>Homeworld: {props.charHomeworld}</p> */}
-            <p>Birth Year: {props.character.birth_year}</p>
+            <p><strong>Birth Year:</strong> {props.character.birth_year}</p>
+            <p><strong>Mass:</strong> {props.character.mass}</p>
+            <p><strong>Hair Color:</strong> {props.character.hair_color}</p>
+            <p><strong>Eye Color:</strong> <span style={{color: props.character.eye_color}}>{props.character.eye_color}</span></p>
         </div>
     );
 };
